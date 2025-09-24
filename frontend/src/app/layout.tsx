e8d7eb8f03c2cb890c51b362d58f0e764719a94b
Fareez Mir <fareezelahi@gmail.com>
2025-09-24 11:43:03 -0400

    added geist

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GeistSans } from 'geist/font/sans'
+
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -15,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.variable}>
+    <html lang="en" className={`${inter.variable} ${GeistSans.variable}`}>
       <body className="font-sans">{children}</body>
     </html>
   )
